0e6a037bc717050856a61f408eff2115f5c4c430
Chaosus <chaosus89@gmail.com>
2024-12-02 20:11:24 +0300

    Fix orphan strings in `EditorHelpBit`

--- editor/editor_help.cpp
+++ editor/editor_help.cpp
@@ -3175,6 +3175,8 @@ DocTools *EditorHelp::get_doc_data() {
 
 #define HANDLE_DOC(m_string) ((is_native ? DTR(m_string) : (m_string)).strip_edges())
 
+SafeNumeric<int> EditorHelpBit::instance_counter;
+
 EditorHelpBit::HelpData EditorHelpBit::_get_class_help_data(const StringName &p_class_name) {
 	if (doc_class_cache.has(p_class_name)) {
 		return doc_class_cache[p_class_name];
@@ -3779,6 +3781,7 @@ void EditorHelpBit::update_content_height() {
 }
 
 EditorHelpBit::EditorHelpBit(const String &p_symbol) {
+	instance_counter.increment();
 	add_theme_constant_override("separation", 0);
 
 	title = memnew(RichTextLabel);
@@ -3804,6 +3807,18 @@ EditorHelpBit::EditorHelpBit(const String &p_symbol) {
 
 	if (!p_symbol.is_empty()) {
 		parse_symbol(p_symbol);
+	}
+}
+
+EditorHelpBit::~EditorHelpBit() {
+	instance_counter.decrement();
+
+	if (instance_counter.get() == 0) {
+		doc_class_cache.clear();
+		doc_property_cache.clear();
+		doc_method_cache.clear();
+		doc_signal_cache.clear();
+		doc_theme_item_cache.clear();
 	}
 }
 
--- editor/editor_help.h
+++ editor/editor_help.h
@@ -32,6 +32,7 @@
 #define EDITOR_HELP_H
 
 #include "core/os/thread.h"
+#include "core/templates/safe_refcount.h"
 #include "editor/code_editor.h"
 #include "editor/doc_tools.h"
 #include "editor/plugins/editor_plugin.h"
@@ -279,6 +280,7 @@ class EditorHelpBit : public VBoxContainer {
 	inline static HashMap<StringName, HashMap<StringName, HelpData>> doc_method_cache;
 	inline static HashMap<StringName, HashMap<StringName, HelpData>> doc_signal_cache;
 	inline static HashMap<StringName, HashMap<StringName, HelpData>> doc_theme_item_cache;
+	static SafeNumeric<int> instance_counter;
 
 	RichTextLabel *title = nullptr;
 	RichTextLabel *content = nullptr;
@@ -318,6 +320,7 @@ public:
 	void update_content_height();
 
 	EditorHelpBit(const String &p_symbol = String());
+	~EditorHelpBit();
 };
 
 // Standard tooltips do not allow you to hover over them.
